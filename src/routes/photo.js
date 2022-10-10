ab61707a66615e75626255fb9332078702e03216
Gabriel-pagnan <gabrielviniciuspagnan@gmail.com>
2022-10-09 22:49:05 -0300

    validação upload

--- src/routes/photo.js
+++ src/routes/photo.js
@@ -1,11 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
+import loginRequired from '../middleware/loginRequired';
 import PhotoController from '../controllers/Photo';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
-router.post('/', upload.single('foto'), PhotoController.store);
+router.post('/', loginRequired, PhotoController.store);
 
 export default router;
