eea2df963fb6ba05b1128146d45517eca60c3e26
Gabriel-pagnan <gabrielviniciuspagnan@gmail.com>
2022-11-08 16:53:10 -0300

    add o express-delay na api

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ dotenv.config();
 import './src/database';
 
 import express from 'express';
+import delay from 'express-delay';
 import cors from 'cors';
 import helmet from 'helmet';
 import { resolve } from 'path';
@@ -37,6 +38,7 @@ class App {
   middlewares() {
     this.app.use(cors(corsOptions));
     this.app.use(helmet());
+    this.app.use(delay(1500));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
